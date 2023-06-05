9f3c12d423f112a67cf141d76097bdb77ba820d0
ismailsahnoun <smil.s@live.fr>
2023-06-05 17:49:49 +0100

    Cross Origin update

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from pydantic import BaseModel
 import joblib
 from app.recommender import base_content_recommendation, collaborative_filtering_recommendation, ensemble_recommendation
@@ -9,13 +10,19 @@ class FreelancerRequest(BaseModel):
     freelancer_id: int
     num_projects: int = 5
 
-# Load your model and any necessary preprocessing steps
-
-# other_script.py
-
 # Load the model
 base_content_recommendation, collaborative_filtering_recommendation, ensemble_recommendation = joblib.load('recommendation_model.joblib')
 
+
+#Cross Origin permission
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Define the prediction endpoint
 @app.post("/predict")
